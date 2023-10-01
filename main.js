3652da51cc96eb1233761b67e55827830598a9fc
Ivan <tipa.ivan0806@gmail.com>
2023-10-02 00:37:59 +0300

    Окончание первого блока. Начало работы со страницами

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 'use strict';
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: () => console.log('hello'),
-};//это большой прототип, который описывает всех солдат
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
+      
 
-john.__proto__ = soldier;//Теперь мы установили прототипом Джона - Солдата
+box.style.cssText = 'background-color: blue; width: 250px;';
 
-Object.setPrototypeOf(john, soldier);//мы установили прототип для Джона от содата.
-//Это если нужно сделать в динамике. То-есть Джон уже существовал, мы ему просто установили прототип.
+btns[1].style.borderRadius = '80%';
+circles[0].style.backgroundColor = 'red';
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
 
-const john = Object.create(soldier);//мы создаем новый объект Jonh, который будет прототипно наследоваться от soldier;
+const div = document.createElement('div');
+
+div.classList.add('black');
+
+wrapper.append(div);
+wrapper.prepend(div);//создает внутри wrapper первым элементом наш div
+
+// hearts[0].before(div);//перед первым блоком .heart создается наш div
+// hearts[0].after(div);//после первого блока .heart создается наш div
+
+// circles[0].remove();//удаляет элемент. В данном случае первый с классом .circle в псевдомассиве circles
+
+hearts[0].replaceWith(circles[0]);//заменяет первый элемент массива hearts на первый элемент массива circles
+
+// document.body.append(div);
+
+// // div.innerHTML = 'Hello World';//можем добавить просто текст, а можем HTML структуру.
+
+// div.innerHTML = '<h1>Hello World</h1>';//добавляем заголовок первого уровня
+
+// // div.textContent = 'hello'; //добавляем обычный текст
+
+// div.insertAdjacentHTML('', '<h2>Hello</h2>');//специальное слово, html который мы хотим вставить
